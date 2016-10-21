b55c9f522a077d47e6c2bdff25d305d2704cd266
Jesse McKay <rosecity27@comcast.net>
2016-10-21 14:10:41 -0700

    finish jquery and javascript functionality

--- js/scripts.js
+++ js/scripts.js
@@ -2,35 +2,41 @@ var addInputs = function(qOneInput, qTwoInput, qThreeInput, qFourInput, qFiveInp
   return (qOneInput + qTwoInput + qThreeInput + qFourInput + qFiveInput);
 };
 
+var numberCheck = function(result) {
+  if (result <= -7) {
+    $(".initial-hidden").hide();
+    $("#css").show();
+  } else if (result > -7 && result <= -3) {
+    $(".initial-hidden").hide();
+    $("#ruby").show();
+  } else if (result > -3 && result <= 3) {
+    $(".initial-hidden").hide();
+    $("#java").show();
+  } else if (result > 3 && result <= 6) {
+    $(".initial-hidden").hide();
+    $("#php").show();
+  } else {
+    $(".initial-hidden").hide();
+    $("#c-sharp").show();
+  }
+}
 
 $(document).ready(function(){
   $("form#questions").submit(function(event){
     event.preventDefault();
-
     var qOneInput = parseInt($("input:radio[name=question1]:checked").val());
     var qTwoInput = parseInt($("input:radio[name=question2]:checked").val());
     var qThreeInput = parseInt($("input:radio[name=question3]:checked").val());
     var qFourInput = parseInt($("input:radio[name=question4]:checked").val());
     var qFiveInput = parseInt($("input:radio[name=question5]:checked").val());
-    var result;
+    
+    var result = addInputs(qOneInput, qTwoInput, qThreeInput, qFourInput, qFiveInput);
 
-    result = addInputs(qOneInput, qTwoInput, qThreeInput, qFourInput, qFiveInput);
-
-
-
-    if($('input:radio:checked').length > 0){
-      alert(result);
+    if ($('input[name="question1"]:checked').length > 0 && $('input[name="question2"]:checked').length > 0 && $('input[name="question3"]:checked').length > 0 && $('input[name="question4"]:checked').length > 0 && $('input[name="question5"]:checked').length > 0) {
+      numberCheck(result);
     } else {
-      alert("Please check all buttons before submitting.");
+      alert("One of more questions has not been answered.");
     }
 
   });
 });
-
-
-
-
-
-
-
-// alert("its working!");
