da3103ee4f59fcda11a0aafbfa454c47fc0c26e2
Jesse McKay <rosecity27@comcast.net>
2016-10-21 11:33:29 -0700

    take form values and alert the data, and check against empty radio buttons, finalize questions to ask

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,31 @@
-$(document).ready(function(){
-  // event.preventDefault();
-});
+var addInputs = function(qOneInput, qTwoInput, qThreeInput, qFourInput, qFiveInput) {
+  return (qOneInput + qTwoInput + qThreeInput + qFourInput + qFiveInput);
+};
 
+
+$(document).ready(function(){
+  $("form#questions").submit(function(event){
+    event.preventDefault();
+
+    var qOneInput = parseInt($("input:radio[name=question1]:checked").val());
+    var qTwoInput = parseInt($("input:radio[name=question2]:checked").val());
+    var qThreeInput = parseInt($("input:radio[name=question3]:checked").val());
+    var qFourInput = parseInt($("input:radio[name=question4]:checked").val());
+    var qFiveInput = parseInt($("input:radio[name=question5]:checked").val());
+    var result;
+
+    result = addInputs(qOneInput, qTwoInput, qThreeInput, qFourInput, qFiveInput);
+
+
+
+    if($('input:radio:checked').length > 0){
+      alert(result);
+    } else {
+      alert("Please check all buttons before submitting.");
+    }
+
+  });
+});
 
 
 
